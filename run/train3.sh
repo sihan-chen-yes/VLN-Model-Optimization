d94ef53ac31846723ff5787bb986c2b1f818e11f
chengaopro <gaochen.ai@gmail.com>
2022-06-20 12:30:35 +0000

    checkout for reverie

--- run/train3.sh
+++ run/train3.sh
@@ -1,9 +1,9 @@
-name=gc-repoduce-r2r
+name=train3-re
 flag="--attn soft --train listener
-      --featdropout 0.3
+      --featdropout 0.3 --load snap/train3/state_dict/best_val_unseen
       --visual_feat --angle_feat
       --feedback sample
-      --mlWeight 0.2
+      --mlWeight 0.2 
       --features clip --gcn_topk 5 --static_gcn_weights --CLIP_language
       --glove_dim 300 --top_N_obj 8 --distance_decay_function same  
       --subout max --dropout 0.5 --optim rms --lr 1e-4 --iters 80000 --maxAction 20"
